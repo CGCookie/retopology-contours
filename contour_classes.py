3c746c953a28ca92dc3ab28356f4e1b65377a6fa
Patrick Moore <patrick.moore.bu@gmail.com>
2014-05-10 11:20:55 -0400

    Reset to default ray_cast behavior to demonstrate to devs to find out if this is API problem
    since new hack was only working some times.
    
    undo code if selected ring is not in the selected path.  This should not happen, but it does
    Will need to further inveistigate

--- contour_classes.py
+++ contour_classes.py
@@ -121,7 +121,7 @@ class ContourCutSeries(object):  #TODO:  nomenclature consistency. Segment, Segm
             view_vector = region_2d_to_vector_3d(region, rv3d, v)
             ray_origin = region_2d_to_origin_3d(region, rv3d, v)
             
-            ray_start = ray_origin - (2000 * view_vector)
+            ray_start = ray_origin # - (2000 * view_vector)
             ray_target = ray_origin + (10000 * view_vector) #TODO: make a max ray depth or pull this depth from clip depth
             hit = ob.ray_cast(imx*ray_start, imx*ray_target)  
                 
@@ -1953,7 +1953,7 @@ class PolySkecthLine(object):
             view_vector = region_2d_to_vector_3d(region, rv3d, v)
             ray_origin = region_2d_to_origin_3d(region, rv3d, v)
             
-            ray_start = ray_origin - (2000 * view_vector) #HACK, sometimes w/o the -2000 correction, rays do not cast
+            ray_start = ray_origin # - (2000 * view_vector) #HACK, sometimes w/o the -2000 correction, rays do not cast
             ray_target = ray_origin + (10000 * view_vector) #TODO: make a max ray depth or pull this depth from clip depth
             hit = ob.ray_cast(imx*ray_start, imx*ray_target)
                 
@@ -3408,7 +3408,7 @@ class ContourCutLine(object):
             
             mx = ob.matrix_world
             imx = mx.inverted()
-            ray_start = ray_origin - (2000 * view_vector)
+            ray_start = ray_origin # - (2000 * view_vector)
             ray_target = ray_origin + (10000 * view_vector) #TODO: make a max ray depth or pull this depth from clip depth
             hit = ob.ray_cast(imx*ray_start, imx*ray_target)
 
@@ -4480,7 +4480,7 @@ class ContourStatePreserver(object):
         else:
             self.selected_path = None
             
-        if operator.selected and operator.selected_path:
+        if operator.selected and operator.selected_path and operator.selected in operator.selected_path:
             self.selected_loop = operator.selected_path.cuts.index(operator.selected)
 
         else:
