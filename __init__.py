e033a70b623b641305da25ec599d2ceea432614a
Jonathan Williamson <jonathan@cgcookie.com>
2014-11-13 15:47:39 -0600

    PEP8 cleanup

--- __init__.py
+++ __init__.py
@@ -46,7 +46,7 @@ from mathutils import Vector
 from mathutils.geometry import intersect_line_plane, intersect_point_line
 
 from bpy.types import Operator, AddonPreferences
-from bpy.props import EnumProperty, StringProperty,BoolProperty, IntProperty, FloatVectorProperty, FloatProperty
+from bpy.props import EnumProperty, StringProperty, BoolProperty, IntProperty, FloatVectorProperty, FloatProperty
 from bpy_extras.view3d_utils import location_3d_to_region_2d, region_2d_to_vector_3d, region_2d_to_location_3d
 
 import contour_utilities
@@ -56,54 +56,57 @@ from contour_classes import ContourCutLine, ExistingVertList, CutLineManipulator
 # Create a class that contains all location information for addons
 AL = general_utilities.AddonLocator()
 
-#a place to store stokes for later
-global contour_cache 
+# A place to store stokes for later
+global contour_cache
 contour_cache = {}
-
 contour_undo_cache = []
 
-#store any temporary triangulated objects
-#store the bmesh to prevent recalcing bmesh
-#each time :-)
+# Store any temporary triangulated objects
+# Store the bmesh to prevent recalcing bmesh each time :-)
 global contour_mesh_cache
 contour_mesh_cache = {}
 
+
 def object_validation(ob):
     me = ob.data
-    
-    # get object data to act as a hash
+
+    # Get object data to act as a hash
     counts = (len(me.vertices), len(me.edges), len(me.polygons), len(ob.modifiers))
-    bbox   = (tuple(min(v.co for v in me.vertices)), tuple(max(v.co for v in me.vertices)))
-    vsum   = tuple(sum((v.co for v in me.vertices), Vector((0,0,0))))
-    
+    bbox = (tuple(min(v.co for v in me.vertices)), tuple(max(v.co for v in me.vertices)))
+    vsum = tuple(sum((v.co for v in me.vertices), Vector((0, 0, 0))))
+
     return (ob.name, counts, bbox, vsum)
+
 
 def is_object_valid(ob):
     global contour_mesh_cache
-    if 'valid' not in contour_mesh_cache: return False
+    if 'valid' not in contour_mesh_cache:
+        return False
     return contour_mesh_cache['valid'] == object_validation(ob)
 
-def write_mesh_cache(orig_ob,tmp_ob, bme):
+
+def write_mesh_cache(orig_ob, tmp_ob, bme):
     print('writing mesh cache')
     global contour_mesh_cache
     clear_mesh_cache()
     contour_mesh_cache['valid'] = object_validation(orig_ob)
     contour_mesh_cache['bme'] = bme
     contour_mesh_cache['tmp'] = tmp_ob
-    
+
+
 def clear_mesh_cache():
     print('clearing mesh cache')
-    
+
     global contour_mesh_cache
-    
+
     if 'valid' in contour_mesh_cache and contour_mesh_cache['valid']:
         del contour_mesh_cache['valid']
-        
+
     if 'bme' in contour_mesh_cache and contour_mesh_cache['bme']:
         bme_old = contour_mesh_cache['bme']
         bme_old.free()
         del contour_mesh_cache['bme']
-    
+
     if 'tmp' in contour_mesh_cache and contour_mesh_cache['tmp']:
         old_obj = contour_mesh_cache['tmp']
         #context.scene.objects.unlink(self.tmp_ob)
@@ -114,104 +117,105 @@ def clear_mesh_cache():
         if old_me and old_me.name in bpy.data.meshes:
             bpy.data.meshes.remove(old_me)
         del contour_mesh_cache['tmp']
-        
+
+
 class ContourToolsAddonPreferences(AddonPreferences):
     bl_idname = __name__
-    
+
     simple_vert_inds = BoolProperty(
-            name="Simple Inds",
-            default=False,
-            )
-    
+        name="Simple Inds",
+        default=False,
+        )
+
     vert_inds = BoolProperty(
-            name="Vert Inds",
-            description = "Display indices of the raw contour verts",
-            default=False,
-            )
-    
+        name="Vert Inds",
+        description="Display indices of the raw contour verts",
+        default=False,
+        )
+
     show_verts = BoolProperty(
-            name="Show Raw Verts",
-            description = "Display the raw contour verts",
-            default=False,
-            )
-    
+        name="Show Raw Verts",
+        description="Display the raw contour verts",
+        default=False,
+        )
+
     show_edges = BoolProperty(
-            name="Show Span Edges",
-            description = "Display the extracted mesh edges. Usually only turned off for debugging",
-            default=True,
-            )
-    
+        name="Show Span Edges",
+        description="Display the extracted mesh edges. Usually only turned off for debugging",
+        default=True,
+        )
+
     show_cut_indices = BoolProperty(
-            name="Show Cut Indices",
-            description = "Display the order the operator stores cuts. Usually only turned on for debugging",
-            default=False,
-            )
-        
-    
+        name="Show Cut Indices",
+        description="Display the order the operator stores cuts. Usually only turned on for debugging",
+        default=False,
+        )
+
     show_ring_edges = BoolProperty(
-            name="Show Ring Edges",
-            description = "Display the extracted mesh edges. Usually only turned off for debugging",
-            default=True,
-            )
-    
+        name="Show Ring Edges",
+        description="Display the extracted mesh edges. Usually only turned off for debugging",
+        default=True,
+        )
+
     draw_widget = BoolProperty(
-            name="Draw Widget",
-            description = "Turn display of widget on or off",
-            default=True,
-            )
-    
+        name="Draw Widget",
+        description="Turn display of widget on or off",
+        default=True,
+        )
+
     debug = IntProperty(
-            name="Debug Level",
-            default=1,
-            min = 0,
-            max = 4,
-            )
-    
+        name="Debug Level",
+        default=1,
+        min=0,
+        max=4,
+        )
+
     show_backbone = BoolProperty(
-            name = "show_backbone",
-            description = "Show Cut Series Backbone",
-            default = False)
-    
+        name="show_backbone",
+        description="Show Cut Series Backbone",
+        default=False)
+
     show_nodes = BoolProperty(
-            name = "show_nodes",
-            description = "Show Cut Nodes",
-            default = False)
-    
+        name="show_nodes",
+        description="Show Cut Nodes",
+        default=False)
+
     show_ring_inds = BoolProperty(
-            name = "show_ring_inds",
-            description = "Show Ring Indices",
-            default = False)
-    
+        name="show_ring_inds",
+        description="Show Ring Indices",
+        default=False)
+
     show_axes = BoolProperty(
-            name = "show_axes",
-            description = "Show Cut Axes",
-            default = False)
-    
+        name="show_axes",
+        description="Show Cut Axes",
+        default=False)
+
     show_debug = BoolProperty(
-            name="Show Debug Settings",
-            description = "Show the debug settings, useful for troubleshooting",
-            default=False,
-            )
-    
+        name="Show Debug Settings",
+        description="Show the debug settings, useful for troubleshooting",
+        default=False,
+        )
+
     show_experimental = BoolProperty(
-            name="Enable Experimental",
-            description = "Enable experimental features and functions that are still in development, useful for experimenting and likely to crash",
-            default=False,
-            )
-    
+        name="Enable Experimental",
+        description="Enable experimental features and functions that are still in development, useful for experimenting and likely to crash",
+        default=False,
+        )
+
     vert_size = IntProperty(
-            name="Vertex Size",
-            default=4,
-            min = 1,
-            max = 10,
-            )
+        name="Vertex Size",
+        default=4,
+        min=1,
+        max=10,
+        )
+
     edge_thick = IntProperty(
-            name="Edge Thickness",
-            default=1,
-            min=1,
-            max=10,
-            )
-    
+        name="Edge Thickness",
+        default=1,
+        min=1,
+        max=10,
+        )
+
     theme = EnumProperty(
         items=[
             ('blue', 'Blue', 'Blue color scheme'),
@@ -221,7 +225,8 @@ class ContourToolsAddonPreferences(AddonPreferences):
         name='theme',
         default='blue'
         )
-    
+
+
     def rgba_to_float(r, g, b, a):
         return (r/255.0, g/255.0, b/255.0, a/255.0)
 
@@ -237,214 +242,213 @@ class ContourToolsAddonPreferences(AddonPreferences):
     }
 
     raw_vert_size = IntProperty(
-            name="Raw Vertex Size",
-            default=1,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Raw Vertex Size",
+        default=1,
+        min=1,
+        max=10,
+        )
+
     handle_size = IntProperty(
-            name="Handle Vertex Size",
-            default=8,
-            min = 1,
-            max = 10,
-            )
+        name="Handle Vertex Size",
+        default=8,
+        min=1,
+        max=10,
+        )
  
-    
     line_thick = IntProperty(
-            name="Line Thickness",
-            default=1,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Line Thickness",
+        default=1,
+        min=1,
+        max=10,
+        )
+
     stroke_thick = IntProperty(
-            name="Stroke Thickness",
-            description = "Width of stroke lines drawn by user",
-            default=1,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Stroke Thickness",
+        description="Width of stroke lines drawn by user",
+        default=1,
+        min=1,
+        max=10,
+        )
+
     auto_align = BoolProperty(
-            name="Automatically Align Vertices",
-            description = "Attempt to automatically align vertices in adjoining edgeloops. Improves outcome, but slows performance",
-            default=True,
-            )
-    
+        name="Automatically Align Vertices",
+        description="Attempt to automatically align vertices in adjoining edgeloops. Improves outcome, but slows performance",
+        default=True,
+        )
+
     live_update = BoolProperty(
-            name="Live Update",
-            description = "Will live update the mesh preview when transforming cut lines. Looks good, but can get slow on large meshes",
-            default=True,
-            )
-    
+        name="Live Update",
+        description="Will live update the mesh preview when transforming cut lines. Looks good, but can get slow on large meshes",
+        default=True,
+        )
+
     use_x_ray = BoolProperty(
-            name="X-Ray",
-            description = 'Enable X-Ray on Retopo-mesh upon creation',
-            default=False,
-            )
-    
+        name="X-Ray",
+        description='Enable X-Ray on Retopo-mesh upon creation',
+        default=False,
+        )
+
     use_perspective = BoolProperty(
-            name="Use Perspective",
-            description = 'Make non parallel cuts project from the same view to improve expected outcome',
-            default=True,
-            )
-    
+        name="Use Perspective",
+        description='Make non parallel cuts project from the same view to improve expected outcome',
+        default=True,
+        )
+
     new_method = BoolProperty(
-            name="New Method",
-            description = "Use robust cutting, may be slower, more accurate on dense meshes",
-            default=True,
-            )
-    
-    #TODO  Theme this out nicely :-) 
+        name="New Method",
+        description="Use robust cutting, may be slower, more accurate on dense meshes",
+        default=True,
+        )
+
+    # TODO  Theme this out nicely :-) 
     widget_color = FloatVectorProperty(name="Widget Color", description="Choose Widget color", min=0, max=1, default=(0,0,1), subtype="COLOR")
     widget_color2 = FloatVectorProperty(name="Widget Color", description="Choose Widget color", min=0, max=1, default=(1,0,0), subtype="COLOR")
     widget_color3 = FloatVectorProperty(name="Widget Color", description="Choose Widget color", min=0, max=1, default=(0,1,0), subtype="COLOR")
     widget_color4 = FloatVectorProperty(name="Widget Color", description="Choose Widget color", min=0, max=1, default=(0,0.2,.8), subtype="COLOR")
     widget_color5 = FloatVectorProperty(name="Widget Color", description="Choose Widget color", min=0, max=1, default=(.9,.1,0), subtype="COLOR")
-    
-    
+
     widget_radius = IntProperty(
-            name="Widget Radius",
-            description = "Size of cutline widget radius",
-            default=25,
-            min = 20,
-            max = 100,
-            )
-    
+        name="Widget Radius",
+        description="Size of cutline widget radius",
+        default=25,
+        min=20,
+        max=100,
+        )
+
     widget_radius_inner = IntProperty(
-            name="Widget Inner Radius",
-            description = "Size of cutline widget inner radius",
-            default=10,
-            min = 5,
-            max = 30,
-            )
-    
+        name="Widget Inner Radius",
+        description="Size of cutline widget inner radius",
+        default=10,
+        min=5,
+        max=30,
+        )
+
     widget_thickness = IntProperty(
-            name="Widget Line Thickness",
-            description = "Width of lines used to draw widget",
-            default=2,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Widget Line Thickness",
+        description="Width of lines used to draw widget",
+        default=2,
+        min=1,
+        max=10,
+        )
+
     widget_thickness2 = IntProperty(
-            name="Widget 2nd Line Thick",
-            description = "Width of lines used to draw widget",
-            default=4,
-            min = 1,
-            max = 10,
-            )
-        
+        name="Widget 2nd Line Thick",
+        description="Width of lines used to draw widget",
+        default=4,
+        min=1,
+        max=10,
+        )
+
     arrow_size = IntProperty(
-            name="Arrow Size",
-            default=12,
-            min=5,
-            max=50,
-            )   
-    
+        name="Arrow Size",
+        default=12,
+        min=5,
+        max=50,
+        )   
+
     arrow_size2 = IntProperty(
-            name="Translate Arrow Size",
-            default=10,
-            min=5,
-            max=50,
-            )      
+        name="Translate Arrow Size",
+        default=10,
+        min=5,
+        max=50,
+        )
+
     vertex_count = IntProperty(
-            name = "Vertex Count",
-            description = "The Number of Vertices Per Edge Ring",
-            default=10,
-            min = 3,
-            max = 250,
-            )
-    
+        name="Vertex Count",
+        description="The Number of Vertices Per Edge Ring",
+        default=10,
+        min=3,
+        max=250,
+        )
+
     cut_count = IntProperty(
-            name = "Ring Count",
-            description = "The Number of Cuts Per Guide Stroke",
-            default=10,
-            min = 3,
-            max = 100,
-            )
-    
-    
+        name="Ring Count",
+        description="The Number of Cuts Per Guide Stroke",
+        default=10,
+        min=3,
+        max=100,
+        )
+
     cyclic = BoolProperty(
-            name = "Cyclic",
-            description = "Make contour loops cyclic",
-            default = False)
-    
+        name="Cyclic",
+        description="Make contour loops cyclic",
+        default=False)
+
     recover = BoolProperty(
-            name = "Recover",
-            description = "Recover strokes from last session",
-            default = False)
-    
+        name="Recover",
+        description="Recover strokes from last session",
+        default=False)
+
     recover_clip = IntProperty(
-            name = "Recover Clip",
-            description = "Number of cuts to leave out, usually set to 0 or 1",
-            default=1,
-            min = 0,
-            max = 10,
-            )
-    
+        name="Recover Clip",
+        description="Number of cuts to leave out, usually set to 0 or 1",
+        default=1,
+        min=0,
+        max=10,
+        )
+
     search_factor = FloatProperty(
-            name = "Search Factor",
-            description = "Factor of existing segment length to connect a new cut",
-            default=5,
-            min = 0,
-            max = 30,
-            )
-        
+        name="Search Factor",
+        description="Factor of existing segment length to connect a new cut",
+        default=5,
+        min=0,
+        max=30,
+        )
+
     intersect_threshold = FloatProperty(
-            name = "Intersect Factor",
-            description = "Stringence for connecting new strokes",
-            default=1.,
-            min = .000001,
-            max = 1,
-            )
-    
+        name="Intersect Factor",
+        description="Stringence for connecting new strokes",
+        default=1.0,
+        min=0.000001,
+        max=1,
+        )
+
     merge_threshold = FloatProperty(
-            name = "Intersect Factor",
-            description = "Distance below which to snap strokes together",
-            default=1.,
-            min = .000001,
-            max = 1,
-            )
-    
+        name="Intersect Factor",
+        description="Distance below which to snap strokes together",
+        default=1.0,
+        min=0.000001,
+        max=1,
+        )
+
     cull_factor = IntProperty(
-            name = "Cull Factor",
-            description = "Fraction of screen drawn points to throw away. Bigger = less detail",
-            default = 4,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Cull Factor",
+        description="Fraction of screen drawn points to throw away. Bigger = less detail",
+        default=4,
+        min=1,
+        max=10,
+        )
+
     smooth_factor = IntProperty(
-            name = "Smooth Factor",
-            description = "Number of iterations to smooth drawn strokes",
-            default = 5,
-            min = 1,
-            max = 10,
-            )
-    
+        name="Smooth Factor",
+        description="Number of iterations to smooth drawn strokes",
+        default=5,
+        min=1,
+        max=10,
+        )
+
     feature_factor = IntProperty(
-            name = "Smooth Factor",
-            description = "Fraction of sketch bounding box to be considered feature. Bigger = More Detail",
-            default = 4,
-            min = 1,
-            max = 20,
-            )
-    
+        name="Smooth Factor",
+        description="Fraction of sketch bounding box to be considered feature. Bigger = More Detail",
+        default=4,
+        min=1,
+        max=20,
+        )
+
     extend_radius = IntProperty(
-            name="Snap/Extend Radius",
-            default=20,
-            min=5,
-            max=100,
-            )
+        name="Snap/Extend Radius",
+        default=20,
+        min=5,
+        max=100,
+        )
 
     undo_depth = IntProperty(
-            name="Undo Depth",
-            default=10,
-            min = 0,
-            max = 100,
-            )
+        name="Undo Depth",
+        default=10,
+        min=0,
+        max=100,
+        )
+
     
     def draw(self, context):
         layout = self.layout
